3d07e95fa09ca855a5fab634000196314da5934a
BarteQ343 <bartoszchmiel72@gmail.com>
2023-04-16 13:26:54 +0200

    Initial creation of a new dialog used to edit metatags

--- v2/downtube2.py
+++ v2/downtube2.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout, QScrollBar, QFileDialog
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout, QScrollBar, QFileDialog
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QTextCursor, QIcon
 import yt_dlp                               # for Downloading from YouTube
@@ -49,10 +49,117 @@ def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
+class NewWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        icon = resource_path("assets/logo.png")
+        # set window properties
+        self.setWindowTitle("YT downloader - Edit metatags")
+        self.setGeometry(100, 100, 570, 600)
+        self.setWindowIcon(QIcon(icon))
+        self.setStyleSheet("""
+            QPushButton {
+                border-style: solid;
+                border-width: 2px;
+                border-color: #B0B0B0;
+                border-radius: 8px;
+                padding: 5px;
+                background-color: #2D2D30;
+                color: #F0F0F0;
+            }
+            QPushButton:hover {
+                border-color: #F0A202;
+                background-color: #4A4A4A;
+           }
+            QPushButton:pressed {
+                border-color: #F0A202;
+                background-color: #F0A202;
+                color: #2D2D30;
+            }
+            QWidget {
+                background-color: #212121;
+                color: #FFFFFF;
+                font-family: Segoe UI, sans-serif;
+                font-size: 14px;
+            }
+            QLabel {
+                font-size: 32px;
+                font-weight: bold;
+            }
+            QLineEdit {
+                background-color: #2c2c2c;
+                color: #FFFFFF;
+                padding: 10px;
+            }
+        """)
+
+    def initUI(self):
+
+        max_width = int(self.width() * 0.95)
+        # Create a big, centered label
+        big_label = QLabel("Edit metatags")
+        big_label.setAlignment(Qt.AlignCenter)
+
+        # Create pairs of small label and text input field
+        label1 = QLabel("Title:")
+        label1.setStyleSheet("font-size: 16px; font-weight: bold; margin-right: 16px")
+        text_input1 = QLineEdit()
+        label2 = QLabel("Artist:")
+        label2.setStyleSheet("font-size: 16px; font-weight: bold; margin-right: 16px")
+        text_input2 = QLineEdit()
+        label3 = QLabel("Year:")
+        label3.setStyleSheet("font-size: 16px; font-weight: bold; margin-right: 16px")
+        text_input3 = QLineEdit()
+        label4 = QLabel("Album:")
+        label4.setStyleSheet("font-size: 16px; font-weight: bold; margin-right: 16px")
+        text_input4 = QLineEdit()
+        label5 = QLabel("Track nr:")
+        label5.setStyleSheet("font-size: 16px; font-weight: bold; margin-right: 16px")
+        text_input5 = QLineEdit()
+
+        # Create two buttons
+        button1 = QPushButton("Reset")
+        button1.setMaximumWidth(max_width)
+        button2 = QPushButton("Done")
+        button2.setMaximumWidth(max_width)
+        button2.clicked.connect(self.accept)
+
+        # Create layout for small label and text input field pairs
+        layout = QVBoxLayout()
+        layout.addWidget(label1)
+        layout.addWidget(text_input1)
+        layout.addWidget(label2)
+        layout.addWidget(text_input2)
+        layout.addWidget(label3)
+        layout.addWidget(text_input3)
+        layout.addWidget(label4)
+        layout.addWidget(text_input4)
+        layout.addWidget(label5)
+        layout.addWidget(text_input5)
+        layout.setSpacing(10)
+
+        # Create layout for buttons
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(button1)
+        button_layout.addWidget(button2)
+        button_layout.setContentsMargins(10, 15, 10, 10)
+        button_layout.setSpacing(10)
+
+        # Create main layout for the new window
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(big_label)
+        main_layout.addLayout(layout)
+        main_layout.addLayout(button_layout)
+
+        # Set the main layout for the window
+        self.setLayout(main_layout)
+
+
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        
+        self.new_window = None
         icon = resource_path("assets/logo.png")
         # set window properties
         self.setWindowTitle("YT downloader")
@@ -245,6 +352,14 @@ class Window(QWidget):
             musicDir = musicDirTmp
             print("Current download location:", musicDir)
 
+    '''def open_new_window(self):
+            #self.new_window = NewWindow()
+            #self.new_window.show()
+            if self.new_window is None:
+                self.new_window = NewWindow()
+            self.new_window.show()'''
+
+            
         
     x = threading.Thread(target=PrintLogger, args=(1,))                                                                 # that's needed for the console to print stuff in real time instead of at the end of the script
     x.start()
@@ -277,6 +392,9 @@ class Window(QWidget):
         
         yt_dlp.YoutubeDL(options).download([video_info['webpage_url']])                                                 # and here's the downloader itself
 
+        dialog = NewWindow()
+        dialog.exec_()
+     
         metatag = EasyID3(filePath+'.mp3')                         # every single line starting with metatag uses mutagen to add metatags. Sample metatags of course, just so it looks good, I always use AutomaTag on Android to actually tag my songs anyway
         metatag['title'] = video_info['title']              # it's really only needed because of yt_dlp borking files when you try to edit metatags later with something like AutomaTag. Might be due to my incorrect installation of FFmpeg tho, idk
         metatag['artist'] = "Unknown"
